5b2ade1a3a4616a1dbdb4a8279924d4e4722a47c
bahrs <131689947+bahrs@users.noreply.github.com>
2025-12-09 22:16:30 +0300

    Update working directory in MLflow launch script
    Changed the working directory path in Launch MLflow.bat to reflect the new location under Documents/_GitHub. This ensures MLflow UI launches from the correct project folder.

--- Launch MLflow.bat
+++ Launch MLflow.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:\Users\Bahrs\YandexDisk\SK_LNM\zzz____LNM\a_Article on SWCNT thermocycling\_Placeholder\Python notebooks\gas-sensor_SWCNT_film-data-classification"
+cd /d "C:\Users\Bahrs\Documents\_GitHub\gas-sensor_SWCNT_film-data-classification"
 call .venv\Scripts\activate
 mlflow ui
 pause
